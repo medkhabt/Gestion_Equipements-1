36f2038cc43fdba53a497d31de4f3320529fe1bf
= <=>
2020-05-05 19:46:59 +0200

    angular-commit-fix-3

--- src-frontend/app/home/home.component.ts
+++ src-frontend/app/home/home.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  cheminImage = 'C:\Users\DELL\Desktop\Angular\Agadir.jpg';
 
   constructor() { }
 
